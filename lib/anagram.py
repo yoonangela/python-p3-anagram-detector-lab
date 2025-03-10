d50f1655333c338666fd7a7d4e45b13c6a2bcf76
Yeji-Yoon <angelayoon0102@gmail.com>
2025-03-10 11:07:11 -0700

    s

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word=word
+
+    def match(self, lists):
+        word = [let for let in self.word]
+        anag=[]
+        for wor in lists:
+            find = [letter for letter in wor]
+            if sorted(find)==sorted(word):
+                anag.append(wor)
+        return anag
+            
+
+
+
+
+# listen = Anagram("listen")
+# listen.match(['enlists', 'google', 'inlets', 'banana'])
+# # => ['inlets']
